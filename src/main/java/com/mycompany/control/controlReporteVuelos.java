95208ff09001648e9ad675074ce69d6adf1203ee
ahzavala <alvaro.zavala@udb.edu.sv>
2017-07-30 14:53:19 -0600

    commit de reportes

---
+++ src/main/java/com/mycompany/control/controlReporteVuelos.java
@@ -0,0 +1,57 @@
+package com.mycompany.control;
+
+import com.mycompany.reportes.Reportes;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+import net.sf.jasperreports.engine.JRException;
+
+
+@ManagedBean
+@ViewScoped
+public class controlReporteVuelos implements Serializable {
+
+    private Date fecha1;
+    private Date fecha2;
+    private String tipo;
+    
+    public controlReporteVuelos() {
+    }
+
+    public Date getFecha1() {
+        return fecha1;
+    }
+
+    public void setFecha1(Date fecha1) {
+        this.fecha1 = fecha1;
+    }
+
+    public Date getFecha2() {
+        return fecha2;
+    }
+
+    public void setFecha2(Date fecha2) {
+        this.fecha2 = fecha2;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void generarReporte() throws JRException, IOException{
+        Reportes r = new Reportes();
+        Map parametros = new HashMap();
+        parametros.put("fecha1", fecha1);
+        parametros.put("fecha2", fecha2);
+        parametros.put("estado", " v.estado in ('DISPONIBLE','NO DISPONIBLE') ");
+   r.generarReporte(parametros, tipo, "/reportes/ReporteVuelos.jasper", "Reporte");
+    }
+}
